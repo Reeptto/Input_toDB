56dea362baaa57bb702c0146c6dab79fc5968545
Reeptto <157365499+Reeptto@users.noreply.github.com>
2025-06-21 12:59:23 +0700

    Add files via upload

---
+++ pertemuan11/viewAdmin/Dosen/tambahDatadosen.php
@@ -0,0 +1,81 @@
+<?php
+include "../header.php";
+include "../koneksi.php";
+
+if (isset($_POST['submit'])) {
+
+    $nama = $_POST['nama'];
+    $nid = $_POST['nid'];
+    $matkul = $_POST['mataKuliah'];
+    $alamat = $_POST['alamat'];
+    
+    $sql = "INSERT into dosen (nama, nid, mataKuliah, alamat) VALUES ('$nama', '$nid', '$matkul', '$alamat')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>Data Berhasil Ditambahkan!<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='close'></button>
+    </div>";
+    }else {
+        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+            Error: '. $sql. '<br>'. $conn->error. <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='close'></button>
+        </div>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Dosen</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+
+    <h1 class="text-center text text-warning">Tambah Data Dosen LP3I</h1>
+
+    <div class="container">
+    <div class="row">
+        <div class="col">
+            <form action="" method="post" class="border border-success p-4 rounded shadow">
+                <div class="mb-3">
+                    <label for="" class="form-label">Nama :</label>
+                    <input type="text" name="nama" class="form-control" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="" class="form-label">NID :</label>
+                    <input type="tel" name="nid" class="form-control" required maxlength="12">
+                </div>
+
+                <div class="mb-3">
+                    <label for="" class="form-label">Mata Kuliah :</label>
+                    <select name="mataKuliah" id="" class="form-control">
+                        <option value="Database">Pilih</option>
+                        <option value="Database">Database</option>
+                        <option value="Web Programming 1">Web Programming 1</option>
+                        <option value="Design Graphic">Design Graphic</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label for="" class="form-label">Alamat :</label>
+                    <textarea name="alamat" id="" class="form-control"></textarea required><br>
+                </div>
+                <div class="row">
+                <div class="col-8"></div> 
+                <div class="col-4 text-end"> 
+                    <button type="submit" class="btn btn-primary mb-3" name="submit">Tambah Data</button>
+                    <a href="/viewAdmin/Dosen/tampilDosen.php">
+                        <button type="button" class="btn btn-secondary mb-3">Kembali</button>
+                    </a>
+                </div>
+              </div>
+                </form>
+            </div>
+    </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ pertemuan11/viewAdmin/Jadwal/jadwal.php
@@ -0,0 +1,77 @@
+    <?php 
+    include "../header.php"; 
+    ?>
+
+    <!-- Header dengan Logout -->
+    <div class="header">
+        <h4>Data Jadwal</h4>
+    </div>
+
+    <!-- Body Content -->
+    <div class="content" style="margin-top: 70px;">
+        <!-- <h1 class='text-center'>Data Mahasiswa ASE10</h1> -->
+        <?php
+        if (isset($_SESSION['alert'])) {
+            echo $_SESSION['alert'];
+            unset($_SESSION['alert']);
+        }
+        ?>
+    <form action="cetakDatamhs.php" method="POST">
+        <button type="submit" class="btn btn-primary float-end"><i class="fa fa-print"></i> Export Data</button>
+    </form>
+
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Mata Kuliah</th>
+                    <th>Dosen</th>
+                    <th>Hari</th>
+                    <th>Jam</th>
+                    <th>Ruangan</th>
+                    <th>Semester</th>
+                    <th>Tahun Ajaran</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    include "../koneksi.php";
+                    $sql = "SELECT a.id, b.namaMatkul, c.namaDosen, d.nama_hari, e.jam_mulai, e.jam_selesai, f.nama_ruangan, semester, tahun_ajaran FROM `jadwal_kuliah` a 
+                    LEFT JOIN matkul b ON a.id_matkul = b.id
+                    LEFT JOIN dosen c ON a.id_dosen = c.id
+                    LEFT JOIN hari d ON a.id_hari = d.id
+                    LEFT JOIN jam_kuliah e ON a.id_jam_kuliah = e.id
+                    LEFT JOIN ruangan f ON a.id_ruangan = f.id";
+
+                    $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        $i = 1;
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<th scope='row'>".$i++."</th>"; 
+                            echo "<td>".$row['namaMatkul']."</td>";
+                            echo "<td>".$row['namaDosen']."</td>";
+                            echo "<td>".$row['nama_hari']."</td>";
+                            echo "<td>".$row['jam_mulai']." - ". $row['jam_selesai']. "</td>";
+                            echo "<td>".$row['nama_ruangan']."</td>";
+                            echo "<td>".$row['semester']."</td>";
+                            echo "<td>".$row['tahun_ajaran']."</td>";
+                            echo "<td>
+                                    <a href='ubahJadwal.php?id=".$row['id']."'>
+                                        <button class='btn btn-warning rounded'>Ubah</button></a> | 
+                                    <a href='prosesHapus.php?id=".$row['id']."'>
+                                        <button class='btn btn-danger rounded'>Hapus</button></a>
+                                </td>";
+                            echo "</tr>";
+                            
+                        }
+                    }
+                ?>
+            </tbody>
+        </table>
+        
+        <a href="tambahJadwal.php"><button type="button" class="form-control btn btn-success mb-3" name="submit">Tambah Data</button></a>
+    </div>
+<?php include "../footer.php"; ?>
