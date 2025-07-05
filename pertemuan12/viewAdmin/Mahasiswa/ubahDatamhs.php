d78aaccea7655be5430eaa4b282d5cd6524121e7
Reeptto <157365499+Reeptto@users.noreply.github.com>
2025-07-05 11:53:54 +0700

    Add files via upload

---
+++ pertemuan12/viewAdmin/Mahasiswa/ubahDatamhs.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<?php 
+    include "../header.php";
+    $target_dir = "../uploads/";
+
+    // Ambil ID mahasiswa yang akan diubah
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = "SELECT * FROM mhs WHERE id = '$id'";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+    }
+
+    if (isset($_POST['update'])) {
+        $nama           = $_POST['nama'];
+        $id_kelas       = $_POST['id_kelas'];
+        $nipd           = $_POST['nipd'];
+        $tanggal_lahir  = $_POST['tanggal_lahir'];
+        $alamat         = $_POST['alamat'];
+
+        // Handle gambar
+      $fileName = basename($_FILES["photoProfile"]["name"]);
+      $uploadOk = 1;
+      $imageFileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+      $target_file = $target_dir. $nama. ".". $imageFileType;
+
+      // Check if file is an actual image
+      $check = getimagesize($_FILES["photoProfile"]["tmp_name"]);
+      if ($check !== false) {
+          $uploadOk = 1;
+      }else {
+          echo "File is not an image.";
+          $uploadOk = 0;
+      }
+
+      # Check file size
+      if ($_FILES["photoProfile"]["size"] > 500000) {
+        echo "Maaf, file terlalu besar";
+        $uploadOk = 0;
+      }
+
+      # Allow certain file formats
+      if (!in_array($imageFileType, ["jpg", "png", "jpeg", "gif"])) {
+      echo "Maaf Hanya JPG, JPEG, PNG, & GIF format yang diperbolehkan";
+      $uploadOk = 0;
+      }
+
+    # Process Upload
+    if ($uploadOk == 1) {
+        if (move_uploaded_file($_FILES["photoProfile"]["tmp_name"], $target_file)) {
+          $photoProfile = $nama. ".". $imageFileType;
+          $sql = "UPDATE mhs SET namaMhs='$nama', id_kelas='$kelas' NIPD='$nipd', tanggalLahir='$tanggal_lahir', photoProfile='$photoProfile', alamat='$alamat' WHERE id='$id'";
+        }else {
+              echo "Sorry, there was an error uploading your file.";
+          }
+    }
+
+        if ($conn->query($sql) === TRUE) {
+            $_SESSION['alert'] = "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                                    Data berhasil Di ubah!
+                                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                                  </div>";
+            $conn->close();
+            header("Location: tampilDatamhs.php");
+            exit();
+        } else {
+            $_SESSION['alert'] = "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                                    Error: ".$sql." - " . $conn->error . "
+                                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                                  </div>";
+            $conn->close();
+            header("Location: tampilDatamhs.php");
+            exit();
+        }
+    }
+
+function buildOption($table, $idField, $nameField) {
+    global $conn;
+    $options = "";
+    $query = "SELECT $idField, $nameField FROM $table";
+    $res = $conn->query($query);
+    while ($r = $res->fetch_assoc()) {
+        $options .= "<option value ='". $r[$idField]."'>". $r[$nameField]."</option>";
+    }
+    return $options;
+}
+?>
+    <div class="header">
+        <h4>Ubah Data Mahasiswa ASE10</h4>
+    </div>
+
+    <div class="content" style="margin-top: 70px;">
+        <form method="POST" class="border p-4 rounded shadow" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="" class="form-label">Nama</label>
+                <input type="text" name="nama" class="form-control" value="<?php echo $row['namaMhs']; ?>" required>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Kelas</label>
+                <select name="id_kelas" class="form-control" id="">
+                    <option value="">-- Pilih Kelas --</option>
+                    <?= buildOption("kelas", "id", "nama_kelas", $row['id_kelas']); ?>
+                </select>
+            </div>
+            
+            <div class="mb-3">
+                <label for="" class="form-label">NIPD</label>
+                <input type="text" name="nipd" class="form-control" maxlength="12" value="<?php echo $row['NIPD']; ?>" required>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Tanggal Lahir</label>
+                <input type="date" name="tanggal_lahir" class="form-control" value="<?php echo $row['tanggalLahir']; ?>" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Foto Profil</label>
+                <img src="../uploads/<?php echo $row['photoProfile']; ?>" alt="Profile Preview" style="width: 150px; height: 150px; object-fit: cover; display: block; margin-bottom: 10px;">
+                <input type="file" name="photoProfile" col="3" class="form-control" required>
+              </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Alamat</label>
+                <textarea name="alamat" class="form-control" required><?php echo $row['alamat']; ?></textarea>
+            </div>
+            <button type="submit" class="btn btn-warning" name="update">Update Data</button>
+        </form>
+    </div>
+
+   <?php include "../footer.php"; ?>
